dfc2fc4488871712e362404b9624b9f46d7020d3
Ginny/Zariem <vramp@student.ethz.ch>
2015-05-16 00:57:42 +0200

    First implementations

--- BlockRespawn/src/dev/zariem/blockrespawn/BlockRespawn.java
+++ BlockRespawn/src/dev/zariem/blockrespawn/BlockRespawn.java
@@ -1,8 +1,8 @@
 package dev.zariem.blockrespawn;
 
-import net.md_5.bungee.api.ChatColor;
-
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -18,8 +18,36 @@ public class BlockRespawn extends JavaPlugin implements Listener {
 		Bukkit.getServer().getPluginManager().registerEvents(new BlockRespawnListener(), this);
 	}
 	
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] arges) {
-		// TODO
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		
+		if (!(sender instanceof Player)){
+			sender.sendMessage(ChatColor.RED + "The console can't use these commands! (For now)");
+		}
+		
+		Player player = (Player) sender;
+		
+		if (cmd.getName().equalsIgnoreCase("br")) {
+			if (args.length == 0) {
+				// TODO add default help page
+				return true;
+			}
+			if (args[0].equalsIgnoreCase("newregion")) {
+				if (args.length < 4) {
+					player.sendMessage(ChatColor.RED + "Wrong number of arguments!"
+							+ "Use /br newregion [name] [material] [respawntime]");
+					return true;
+				}
+				if (!isInteger(args[3])) {
+					player.sendMessage(ChatColor.RED + "Make sure that the respawn time is a number!");
+					return true;
+				}
+				int time = Integer.parseInt(args[3]);
+				// TODO probably add a check for the validity of the material name.
+				Material mat = Material.getMaterial(args[2]);
+				BlockRespawnRegion newRegion = new BlockRespawnRegion(args[1], player.getWorld(), mat, time);
+				// TODO continue coding!
+			}
+		}
 		return true;
 	}
 	
@@ -29,4 +57,17 @@ public class BlockRespawn extends JavaPlugin implements Listener {
 		// TODO
 	}
 
+	public static boolean isInteger(String s) {
+		if (s.isEmpty()) {
+			return false;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			// checks whether the character is a digit from 0 to 9
+			if(Character.digit(s.charAt(i), 10) < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }
--- BlockRespawn/src/dev/zariem/blockrespawn/BlockRespawnRegion.java
+++ BlockRespawn/src/dev/zariem/blockrespawn/BlockRespawnRegion.java
@@ -1,5 +1,70 @@
 package dev.zariem.blockrespawn;
 
-public class BlockRespawnRegion {
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
+public class BlockRespawnRegion {
+	/*
+	 * This defines a cuboid region in which a certain
+	 * block type has a certain respawning time.
+	 */
+	
+	final String name;
+	final Material material;
+	final World world;
+	final long respawnSeconds;
+	int x1, y1, z1, x2, y2, z2;
+	
+	public BlockRespawnRegion(String n, World w, Material m, long respawnSeconds) {
+		// constructor
+		this.name = n;
+		this.world = w;
+		this.material = m;
+		this.respawnSeconds = respawnSeconds;
+	}
+	
+	public void defineFirstCorner(Block block) {
+		// used for selecting the region
+		if (block.getWorld() != this.world) {
+			Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED +
+					"The chosen block is not in the same world as your region!");
+			return;
+		}
+		this.x1 = block.getX();
+		this.y1 = block.getY();
+		this.z1 = block.getZ();
+	}
+	
+	public void defineSecondCorner(Block block) {
+		// used for selecting the region
+		if (block.getWorld() != this.world) {
+			Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED +
+					"The chosen block is not in the same world as your region!");
+			return;
+		}
+		this.x2 = block.getX();
+		this.y2 = block.getY();
+		this.z2 = block.getZ();
+	}
+	
+	// Getter functions
+	public String getName() {
+		return this.name;
+	}
+	
+	public World getWorld() {
+		return this.world;
+	}
+	
+	public Material getMaterial() {
+		return this.material;
+	}
+	
+	public long getRespawnTime() {
+		return this.respawnSeconds;
+	}
 }
